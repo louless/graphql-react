168ff65c85acf47429d96c01a82af69063ee5e46
VLadLukin <lvg135@yandex.ru>
2017-08-09 09:33:35 +0400

    ajax query for CORS

--- src/components/blotter/Blotter.jsx
+++ src/components/blotter/Blotter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 const ApiSvc = require('../../../assets/js/services/tradingServer/apiSvc.js');
-
+const config = require('../../../assets/js/services/tradingServer/config.js');
 
 const Blotter = () => {
     
@@ -36,7 +36,37 @@ const Blotter = () => {
         ApiSvc.testCorsGraphqlServer();
         return;
     }
-    
+
+    function test$Click(){
+//        $.ajax({
+//
+//            url : config.dataMgmtUrl,
+//            type : 'GET',
+////            data : {
+////                'numberOfWords' : 10
+////            },
+//            dataType:'text/html',
+//            success : function(data) {
+//                alert('Data: '+data);
+//            },
+//            error : function(request, error)
+//            {
+//                alert("Request: "+JSON.stringify(request));
+//            }
+//        });
+//
+        var jqxhr =
+            $.ajax({
+                url: config.dataMgmtUrl
+//                data: {
+//                    name : "The name",
+//                    desc : "The description"
+//                }
+            })
+            .done  (function(data, textStatus, jqXHR)        { alert("Success: " + data) ; })
+            .fail  (function(jqXHR, textStatus, errorThrown) { alert("Error")   ; })
+            .always(function(jqXHROrData, textStatus, jqXHROrErrorThrown)     { alert("complete"); });
+    }
     
     
   return (
@@ -44,9 +74,10 @@ const Blotter = () => {
       <div className="panel-heading">BLOTTER</div>
       <div className="panel-body">
         Panel content
-        <button className="btn" onClick={testClick}>test CORS marx app</button>
-        <button className="btn" onClick={loginClick}>do log in as test</button>  
-        <button className="btn" onClick={testCorsGraphqlServerClick}>test CORS graphqlserver app</button>
+        <button className="btn className='btn btn-primary btn-block" onClick={testClick}>test CORS marx app</button>
+        <button className="btn className='btn btn-primary btn-block" onClick={test$Click}>test CORS marx app using JQuery</button>
+        <button className="btn className='btn btn-primary btn-block" onClick={loginClick}>do log in as test</button>
+        <button className="btn className='btn btn-primary btn-block" onClick={testCorsGraphqlServerClick}>test CORS graphqlserver app</button>
       </div>
     </div>
   );
